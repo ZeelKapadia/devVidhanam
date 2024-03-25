6db9caa23d5abd199a58aba1c5fb073eb7675b2e
Vijay Prajapati <vijay.prajapati.0713@gmail.com>
2024-03-25 20:32:16 +0530

    changes

--- src/app/modules/home/why-choose-us/why-choose-us.component.ts
+++ src/app/modules/home/why-choose-us/why-choose-us.component.ts
@@ -6,5 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./why-choose-us.component.scss']
 })
 export class WhyChooseUsComponent {
+  dataLists = [
+    {
+      thumbImage: "/assets/img/why_us_1.jpg",
+      icon: "/assets/img/icon/employees.png",
+      title: "Friendly Environment",
+      description: "Timeam nusquam usu ut, civibus fabellas eleifend eu vis. Id ipsum civibus pri, te ignota dignissim mel, iisque recteque."
+    },
+    {
+      thumbImage: "/assets/img/why_us_2.jpg",
+      icon: "/assets/img/icon/statistics.png",
+      title: "Expert Instructor",
+      description: "Timeam nusquam usu ut, civibus fabellas eleifend eu vis. Id ipsum civibus pri, te ignota dignissim mel, iisque recteque."
+    },
+    {
+      thumbImage: "/assets/img/why_us_3.jpg",
+      icon: "/assets/img/icon/locked.png",
+      title: "Group Teaching",
+      description: "Timeam nusquam usu ut, civibus fabellas eleifend eu vis. Id ipsum civibus pri, te ignota dignissim mel, iisque recteque."
+    },
+    {
+      thumbImage: "/assets/img/why_us_1.jpg",
+      icon: "/assets/img/icon/employees.png",
+      title: "Friendly Environment",
+      description: "Timeam nusquam usu ut, civibus fabellas eleifend eu vis. Id ipsum civibus pri, te ignota dignissim mel, iisque recteque."
+    },
+    {
+      thumbImage: "/assets/img/why_us_2.jpg",
+      icon: "/assets/img/icon/statistics.png",
+      title: "Expert Instructor",
+      description: "Timeam nusquam usu ut, civibus fabellas eleifend eu vis. Id ipsum civibus pri, te ignota dignissim mel, iisque recteque."
+    },
+    {
+      thumbImage: "/assets/img/why_us_3.jpg",
+      icon: "/assets/img/icon/locked.png",
+      title: "Group Teaching",
+      description: "Timeam nusquam usu ut, civibus fabellas eleifend eu vis. Id ipsum civibus pri, te ignota dignissim mel, iisque recteque."
+    },
+  ];
 
+  slideConfig = {
+    "slidesToShow": 3,
+    "slidesToScroll": 1,
+    "dots": false,
+    "infinite": true,
+    "autoplay": true,
+    "autoplaySpeed": 2500,
+    "prevArrow": false,
+    "nextArrow": false,
+  };
 }
